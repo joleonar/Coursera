dc02de8c2be120c85a1af28d5f010a96b2c3021e
jletteboer <john@networkx.nl>
2015-06-21 16:24:39 +0200

    Some updates

--- Getting and Cleaning Data/run_analysis.R
+++ Getting and Cleaning Data/run_analysis.R
@@ -1,8 +1,8 @@
 ## Author: John Letteboer
 ## Date: June 19, 2015
-## --------------------------------------------
-##  Getting and Cleaning Data : Course Project 
-## --------------------------------------------
+## -------------------------------------------
+##  Getting and Cleaning Data: Course Project 
+## -------------------------------------------
 ## 1. Merges the training and the test sets to create one data set.
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 ## 3. Uses descriptive activity names to name the activities in the data set
